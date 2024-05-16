790bd0e591f0139630340b1d52b5b57a6d1e2fb8
Mariahgasper2126 <97246926+Mariahgasper2126@users.noreply.github.com>
2024-05-16 09:49:19 +0530

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Button1.Text = "submit"
+
+        Button2.Text = "submit"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("HII")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("GOOD MORNING")
+
+    End Sub
+
+End Class
